c0a70bf4c7eaa81657effc479ce12ad7986f277a
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-09-24 21:10:19 +1200

    Configure mini.surround

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -1,10 +1,30 @@
 ---@type LazySpec
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {},
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	},
+	{
+		"echasnovski/mini.surround",
+		version = "*",
+		opts = function()
+			require("mini.surround").setup({
+				mappings = {
+					add = "gsa",       -- Add surrounding in Normal and Visual modes
+					delete = "gsd",    -- Delete surrounding
+					find = "gsf",      -- Find surrounding (to the right)
+					find_left = "gsF", -- Find surrounding (to the left)
+					highlight = "gsh", -- Highlight surrounding
+					replace = "gsr",   -- Replace surrounding
+					update_n_lines = "gsn", -- Update `n_lines`
+
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
+			})
+		end,
+	},
 }

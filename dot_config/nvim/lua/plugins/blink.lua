03c4ca09591776ad74708825514ac10603158c1d
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-12-16 20:37:14 +1300

    neovim: Enable round borders for blink
    Update .config/nvim/lua/plugins/blink.lua

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,24 @@
+return {
+  "saghen/blink.cmp",
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    completion = {
+      documentation = {
+        window = {
+          border = "rounded",
+        },
+      },
+      menu = {
+        border = "rounded",
+      },
+    },
+
+    signature = {
+      window = {
+        border = "rounded",
+      },
+    },
+  },
+}

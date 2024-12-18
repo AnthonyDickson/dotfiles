860c6da13291ae1c72704af841bf38b145d8c4fa
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-12-18 21:09:29 +1300

    neovim: Set blink to manually select items in autocomplete menu
    I spent too long undoing text inserted by accidental autocompletions.

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,24 +1,27 @@
 return {
-  "saghen/blink.cmp",
+	"saghen/blink.cmp",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    completion = {
-      documentation = {
-        window = {
-          border = "rounded",
-        },
-      },
-      menu = {
-        border = "rounded",
-      },
-    },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		completion = {
+			list = {
+				selection = "manual",
+			},
+			documentation = {
+				window = {
+					border = "rounded",
+				},
+			},
+			menu = {
+				border = "rounded",
+			},
+		},
 
-    signature = {
-      window = {
-        border = "rounded",
-      },
-    },
-  },
+		signature = {
+			window = {
+				border = "rounded",
+			},
+		},
+	},
 }

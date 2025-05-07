5a02c06454e0f0d2a718795d83fa8ad31910098d
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-05-07 20:41:28 +1200

    m93p: Add hypridle

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -104,6 +104,33 @@
     };
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";      # avoid starting multiple hyprlock instances.
+        before_sleep_cmd = "loginctl lock-session";   # lock before suspend.
+        after_sleep_cmd = "hyprctl dispatch dpms on"; # avoid having to press a key twice to turn on the display.
+      };
+
+      listener = [
+        {
+          timeout = 900;                        # 15 min
+          on-timeout = "loginctl lock-session"; # lock screen on timeout
+        }
+        {
+          timeout = 930;                            # 15.5 min
+          on-timeout = "hyprctl dispatch dpms off"; # screen off on timeout
+          on-resume = "hyprctl dispatch dpms on";   # screen on on activity
+        }
+        {
+          timeout = 930;                    # 15.5 min
+          on-timeout = "systemctl suspend"; # suspend pc
+        }
+      ];
+    };
+  };
+
   services = {
     dunst.enable = true;
     hyprpolkitagent.enable = true;

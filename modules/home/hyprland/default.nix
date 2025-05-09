7e6f01e5d1d1f395f845524ee300554f7e7a2e28
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-05-09 21:46:04 +1200

    m93p: Add basic waybar config

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -32,6 +32,7 @@
       exec-once = [
         "walker --gapplication-service"
         "dunst"
+        "waybar"
       ];
     };
   };
@@ -121,7 +122,7 @@
 
       listener = [
         {
-          timeout = 900;                        # 15 min
+          timeout = 300;                        # 5 min
           on-timeout = "loginctl lock-session"; # lock screen on timeout
         }
         {
@@ -135,6 +136,80 @@
         }
       ];
     };
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        modules-left = ["hyprland/workspaces" "hyprland/window"];
+        modules-center = ["clock"];
+        modules-right = ["cpu" "memory" "network" "pulseaudio"];
+
+        "hyprland/workspaces" = {
+          persistent-workspaces = {
+            "*" = 4;
+          };
+        };
+        "hyprland/window" = {
+          format = "{class} - {title}";
+        };
+
+        clock = {
+          format = "{:%a, %d. %b %H:%M}";
+        };
+
+        cpu = {
+           interval = 5;
+           format = "CPU {icon0}{icon1}{icon2}{icon3}";
+           format-icons = [
+              "<span color='#69ff94'>▁</span>"
+              "<span color='#2aa9ff'>▂</span>"
+              "<span color='#f8f8f2'>▃</span>"
+              "<span color='#f8f8f2'>▄</span>"
+              "<span color='#ffffa5'>▅</span>"
+              "<span color='#ffffa5'>▆</span>"
+              "<span color='#ff9977'>▇</span>"
+              "<span color='#dd532e'>█</span>"
+           ];
+        };
+        memory = {
+          interval = 10;
+          format = "RAM {used:0.1f}G/{total:0.1f}G";
+        };
+        network = {
+          interval = 5;
+          family = "ipv4";
+          # TODO: Print ipv4 address on local network
+          format = "{ifname} ↓ {bandwidthDownBytes} ↑ {bandwidthUpBytes}";
+          format-disconnected = "No Internet Connection";
+        };
+
+        pulseaudio = {
+            format = "{volume}% {icon}";
+            format-bluetooth = "{volume}% {icon}";
+            # TODO: Fix way showing mute icon when computer is not muted
+            format-muted =  "";
+            format-icons =  {
+                "alsa_output.pci-0000_00_1f.3.analog-stereo" =  "";
+                "alsa_output.pci-0000_00_1f.3.analog-stereo-muted" =  "";
+                headphone =  "";
+                hands-free =  "";
+                headset =  "";
+                phone =  "";
+                phone-muted =  "";
+                portable =  "";
+                car =  "";
+                default =  ["" ""];
+            };
+            scroll-step = 1;
+            # TODO: Add on click to open audio management program
+        };
+      };
+    };
+    style = ./waybar.css;
   };
 
   services = {

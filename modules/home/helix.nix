073b0f3393749c00f8be568a2f13fb8c49f3d0db
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-05-11 18:06:58 +1200

    m93p: Update helix config

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -4,9 +4,20 @@
     defaultEditor = true;
     settings = {
       editor = {
+        bufferline = "multiple";
         line-number = "relative";
+        rulers = [80 120];
         cursor-shape = {
           insert = "bar";
+        };
+        statusline = {
+          left = ["mode" "spinner" "version-control" "file-name" "read-only-indicator" "file-modification-indicator"];
+          right = ["diagnostics" "selections" "register" "position" "total-line-numbers" "file-encoding"];
+        };
+        end-of-line-diagnostics = "hint";
+        inline-diagnostics = {
+          cursor-line = "error";
+          other-lines = "disable";
         };
       };
       keys.normal = {

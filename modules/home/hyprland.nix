a0e981046b03fe2b29224ed6451487dfac882195
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-06-07 09:19:29 +1200

    m93p: Add Hyprland monitor config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -3,6 +3,9 @@
     enable = true;
     settings = {
       "$mod" = "SUPER";
+      monitor = [
+        "DP-1, 3840x2160@60, 0x0, 1.5"
+      ];
       bind = [
         "$mod, W, exec, firefox"
         "$mod, K, exec, kitty"

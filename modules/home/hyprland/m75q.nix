9d1e0d53b77467889927f28d33e4a0a8fd3630bb
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-06-20 09:46:10 +1200

    m75q: Assign workspaces to monitors and set up keymaps

--- modules/home/hyprland/m75q.nix
+++ modules/home/hyprland/m75q.nix
@@ -1,3 +1,7 @@
+let
+  mainMonitor = "desc:Acer Technologies XB273K GP 0x0511FB48";
+  secondaryMonitor = "desc:AOC Q27G2G4 0x0000175D";
+in
 {
   imports = [
     ./default.nix
@@ -6,8 +10,28 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "desc:Acer Technologies XB273K GP 0x0511FB48, 3840x2160@60, 0x0, 1.5"
-        "desc:AOC Q27G2G4 0x0000175D, 2560x1440@60, auto-left, 1.0, transform, 3"
+        "${mainMonitor}, 3840x2160@60, 0x0, 1.5"
+        "${secondaryMonitor}, 2560x1440@60, auto-left, 1.0, transform, 3"
+      ];
+      workspace = [
+        "1, monitor:${mainMonitor}, persistent:true"
+        "2, monitor:${mainMonitor}, persistent:true"
+        "3, monitor:${mainMonitor}, persistent:true"
+        "4, monitor:${mainMonitor}, persistent:true"
+        "5, monitor:${secondaryMonitor}, persistent:true"
+        "6, monitor:${secondaryMonitor}, persistent:true"
+        "7, monitor:${secondaryMonitor}, persistent:true"
+        "8, monitor:${secondaryMonitor}, persistent:true"
+      ];  
+      bind = [
+        "$mod, Z, workspace, 5"
+        "$mod, X, workspace, 6"
+        "$mod, C, workspace, 7"
+        "$mod, D, workspace, 8"
+        "$mod SHIFT, Z, movetoworkspace, 5"
+        "$mod SHIFT, X, movetoworkspace, 6"
+        "$mod SHIFT, C, movetoworkspace, 7"
+        "$mod SHIFT, D, movetoworkspace, 8"
       ];
     };
   };

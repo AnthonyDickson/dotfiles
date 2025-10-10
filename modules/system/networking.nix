6bd9030a7e9745df377cad071f8f490bdeec14dc
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-10-11 09:15:08 +1300

    Add localsend to allowlist in firewall

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,7 +1,15 @@
-{hostname, ...}:
+{ hostname, ... }:
+let
+  localsend = 53317;
+in
 {
   networking.hostName = hostname; # Define your hostname.
 
   # Enable networking
-  networking.networkmanager.enable = true; 
+  networking.networkmanager.enable = true;
+
+  networking.firewall = {
+    allowedTCPPorts = [ localsend ];
+    allowedUDPPorts = [ localsend ];
+  };
 }

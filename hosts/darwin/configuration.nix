ac8627dfca61f95f4bb981208fa9cbf7b0a27663
Anthony Dickson <anthony.dickson9656@gmail.com>
2026-06-09 18:30:22 +1200

    darwin: Remove borders

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -101,12 +101,4 @@
     ];
     onActivation.cleanup = "zap";
   };
-
-  services.jankyborders = {
-    enable = true;
-    width = 4.0;
-    active_color = "0xffe1e3e4";
-    inactive_color = "0xff494d64";
-    hidpi = true;
-  };
 }
